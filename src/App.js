df933654a765bd35624da3e6babb6605fd781ba8
Kwaku Dapaah <kwa2the4@gmail.com>
2018-09-16 21:12:41 -0400

    changes made again

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: ['Iron-man', 'Black Widow', 'Hawkeye', 'Hulk', 'Thor']
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Avengers: by Kwaku</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.avengers.map(avenger => <div key={avenger}>{avenger}</div>)}
       </div>
     );
   }
